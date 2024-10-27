96569f8593adac7312e02f62ae0cb7a3230a2677
MKRashed <mkrashed757@gamil.com>
2024-10-27 13:14:42 +0600

    versel build problem

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,20 @@ import { ProjectContext } from './context'
 import { initialSearch, searchReducer } from './reducers/searchReducer'
 
 function App() {
-  const  [ state, dispatch ] = useReducer(searchReducer, initialSearch );
-
+  const  [ state, dispatch ] = useReducer(searchReducer, initialSearch);
   return (
     <>
-    <ProjectContext.Provider value={{ state, dispatch }} >
-        <div className="bg-gray-900 text-white">
-          <div className="flex h-screen">
-          <SideBar/>
-          <main className='flex-1 overflow-y-auto overflow-x-hidden'>
-              <Header/>
-              <ProjectList/>
-          </main>
+      <ProjectContext.Provider value={{ state, dispatch }} >
+          <div className="bg-gray-900 text-white">
+            <div className="flex h-screen">
+            <SideBar/>
+            <main className='flex-1 overflow-y-auto overflow-x-hidden'>
+                <Header/>
+                <ProjectList/>
+            </main>
+            </div>
           </div>
-        </div>
-    </ProjectContext.Provider>
+      </ProjectContext.Provider>
     </>
   )
 }
